4756f33626aaa2afd28e25053a2e3af98e3a9807
haneenusama20 <haneenusama101@gmail.com>
2025-12-04 19:09:09 +0200

    handling interfaces

---
+++ src/Model/Handler.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import Controller.Control;
+import Controller.Controllable;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author DELL
+ */
+public class Handler implements Viewable{
+ 
+    private Load load;
+    private Catalog catalog;
+    private Controllable control; 
+
+    public Handler() {
+        load = new Load();
+        control = new Control(); 
+        catalog = new Catalog(false,false);
+    }
+
+    @Override
+    public Catalog getCatalog() {
+        return catalog;
+    }
+
+    @Override
+    public Game getGame(DifficultyEnum level) throws NotFoundException {
+       
+        switch (level) {
+            case EASY:
+                return load.loadEasyGame(); 
+            case MEDIUM:
+                return load.loadMediumGame();
+            case HARD:
+                return load.loadHardGame();
+            default:
+                JOptionPane.showMessageDialog(null, "ERRORRR!! CANNOT FIND THE DIFFICULTY");
+                return null;
+        }
+    }
+
+    @Override
+    public void driveGames(Game source) throws SolutionInvalidException {
+        if (source == null) return;
+        control.driveGames(source.getBoard());
+    }
+
+    @Override
+    public String verifyGame(Game game) {
+        return null; 
+    }
+
+    @Override
+    public int[] solveGame(Game game) throws InvalidGame {
+        return null; 
+    }
+
+    @Override
+    public void logUserAction(String userAction) throws IOException {
+       
+    }
+}
+
