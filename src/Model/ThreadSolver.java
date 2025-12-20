cd72fec316c159d653cd202210f21aaf4d19b58c
nourelashwah <elashwahnour@gmail.com>
2025-12-20 15:57:23 +0200

    8yrt haga f thread solver

--- src/Model/ThreadSolver.java
+++ src/Model/ThreadSolver.java
@@ -4,7 +4,7 @@
  */
 package Model;
 
-import Backend.zerothreads;
+import Model.Verifier;
 import Model.ThreadManger;
 import java.util.List;
 
@@ -12,13 +12,14 @@ import java.util.List;
  *
  * @author LapTop
  */
-public class ThreadSolver implements Runnable
-{
+public class ThreadSolver implements Runnable {
+
     private int start;
     private int end;
-    private int [][]board;
+    private int[][] board;
     private List<int[]> empty;
     private ThreadManger threadManger;
+    private Verifier verifier;
 
     public ThreadSolver(int start, int end, int[][] board, List<int[]> empty, ThreadManger threadManger) {
         this.start = start;
@@ -27,50 +28,62 @@ public class ThreadSolver implements Runnable
         this.empty = empty;
         this.threadManger = threadManger;
     }
+
     @Override
-    public void run()
-    {
-        PermutationIterator iterator=new PermutationIterator(new PermutationFlyWeight(5));
-        for(int i=0;i<start;i++)// bafdl a skip le7ad ma ywsl lel start index
+    public void run() {
+        PermutationIterator iterator = new PermutationIterator(new PermutationFlyWeight(5));
+        for (int i = 0; i < start; i++)// bafdl a skip le7ad ma ywsl lel start index
         {
-        if(iterator.hasNext())
-        {
-          iterator.next();
+            if (iterator.hasNext()) {
+                iterator.next();
+            }
         }
+        int count = start;
+        while (iterator.hasNext() && count <= end && !threadManger.isSolutionFound() && !Thread.currentThread().isInterrupted()) {
+            int[] combination = iterator.next();
+            int[][] boardTemp = copyBoard(board);
+            fillEmpty(boardTemp, combination);
+            if (isBoardValid(boardTemp)) {
+                threadManger.notify(boardTemp);
+                return;//3ashn 5las fe solution was found
+            }
+            count++;
+
         }
-    int count=start;
-    while(iterator.hasNext()&&count<=end&&! threadManger.isSolutionFound()&&!Thread.currentThread().isInterrupted())
-    {
-        int []combination =iterator.next();
-        int [][] boardTemp=copyBoard(board);
-        fillEmpty(boardTemp, combination);
-        if(zerothreads.isBoardValid(boardTemp))
-        {
-            threadManger.notify(boardTemp);
-            return;//3ashn 5las fe solution was found
-                }
-        count++;
-            
-        
     }
-    }
+
     private int[][] copyBoard(int[][] og)//so each thread has their own copy of the board
     {
-    int[][]copy=new int[9][9];
-    for(int i=0;i<9;i++)
-    {
-        for (int j=0;j<9;j++) {
-            copy[i][j]=og[i][j];
+        int[][] copy = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                copy[i][j] = og[i][j];
+            }
+        }
+        return copy;
+    }
+
+    private void fillEmpty(int[][] board, int[] combo) {
+        for (int i = 0; i < 5; i++) {
+            int r = empty.get(i)[0];
+            int c = empty.get(i)[1];
+            board[r][c] = combo[i];
         }
     }
-    return copy;
-}
-    private void fillEmpty(int[][] board, int[] combo) {
-    for (int i=0;i<5;i++) {
-        int r=empty.get(i)[0];
-        int c=empty.get(i)[1];
-        board[r][c]=combo[i];
+
+    private boolean isBoardValid(int[][] tempBoard) {
+        boolean valid[][] = verifier.verify(tempBoard);
+        if (valid == null) {
+            return false; //le7ad ma ashof fe exception or not
+        }
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (!valid[r][c]) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
     }
-}
-    
 }
